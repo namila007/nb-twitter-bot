26cc5c54096f5919daecbc5cc41b10fc21c54ed2
Namila Bandara <namila007@gmail.com>
2018-07-14 04:22:04 +0530

    added procfile

--- app.js
+++ app.js
@@ -4,6 +4,43 @@ const twit = require('twit')
 
 const twitter = new twit(auth)
 
-twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-    console.log(data)
+
+// twitter.post('statuses/update', { status: 'hello world1!' }, function(err, data, response) {
+//     console.log(data)
+//   })
+
+  var params = {
+    q: 'from%3Anamila007',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+  }
+
+  var getmine = {
+    screen_name: 'namila007',
+    count: 2
+  }
+
+twitter.get('search/tweets', params, function(err, data, response) {
+    if(!err){
+        var i=0;
+        while(i <10){
+            console.log(data.statuses[i].text)
+            i++
+        }
+    } else {
+      console.log(err)
+    }
+  })
+
+  twitter.get('statuses/user_timeline', params, function(err, data, response) {
+    if(!err){
+        var i=0;
+        
+            console.log(data)
+           
+        
+    } else {
+      console.log(err)
+    }
   })
